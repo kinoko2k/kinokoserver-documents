d638e9f5f7e342c79d92eadd26f3b71b2429d707
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2023-05-16 22:52:33 +0900

    fix: description

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} | @kinoko2k`}
+      description="KinokoServer Documents">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
