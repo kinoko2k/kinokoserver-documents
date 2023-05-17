0ff538a381f8cf68c288f7340a5c01b03a5d4cea
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2023-05-17 15:08:34 +0900

    fix: add description

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            さっそく、読みましょう！
           </Link>
         </div>
       </div>
